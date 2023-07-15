0437f8ee30bbe7725404aaaccacd14de45f5fe08
allyleandre <tuyambazeleandre@gmail.com>
2023-07-15 18:08:36 +0200

    ch(): setup swagger documentation

---
+++ src/documentation/index.js
@@ -0,0 +1,9 @@
+import basicInfo from "./basicInfo";
+import welcomeRouteDocs from "./welcome.docs";
+
+export default {
+  ...basicInfo,
+  paths: {
+    ...welcomeRouteDocs,
+  },
+};
---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+import express from "express";
+import welcomeRoutes from "./api/welcome.routes";
+
+const routes = express.Router();
+
+routes.use("/", welcomeRoutes);
+
+export default routes;
