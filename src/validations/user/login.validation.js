6ad1d25dad2adb1b5974a347a6e71798bef43f38
allyleandre <tuyambazeleandre@gmail.com>
2023-07-25 15:23:24 +0200

    ft(): refactor error validation

--- src/validations/user/login.validation.js
+++ src/validations/user/login.validation.js
@@ -1,19 +1,19 @@
 import Joi from 'joi';
 import { joiPasswordExtendCore } from 'joi-password';
+import validateRequest from '../../utils/validation.util';
 const joiPassword = Joi.extend(joiPasswordExtendCore);
 
 const validateLogin = async (req, res, next) => {
   const loginSchema = Joi.object().keys({
-    email: Joi.string().required(),
+    email: Joi.string().email().required(),
     password: joiPassword.string().required(),
   });
 
-  const { error } = loginSchema.validate(req.body);
-  if (error) {
-    return res.status(400).json({
-      error: error.details[0].message.replace(/[^a-zA-Z0-9 ]/g, ''),
-    });
+  const validationError = validateRequest(loginSchema, req.body);
+  if (validationError) {
+    return res.status(400).json({ error: validationError });
   }
+
   next();
 };
 
